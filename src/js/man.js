aa85861c0c11d38a3ba716ed9beaddf6d907f218
LapPavel <wizd@tut.by>
2024-04-05 16:38:03 +0300

    add man

--- src/js/man.js
+++ src/js/man.js
@@ -8,39 +8,44 @@ export function addHead(element) {
 }
 
 export function addBody(element) {
-  const rect = document.createElementNS(SVG_NS, 'rect');
-  rect.setAttribute('x', '130');
-  rect.setAttribute('y', '90');
-  rect.setAttribute('width', '20');
-  rect.setAttribute('height', '50');
-  rect.setAttribute('style', 'stroke: #000; stroke-width: 2; fill: #fff;');
-  element.appendChild(rect);
+  element.insertAdjacentHTML(
+    'beforeend',
+    '<line x1="140" y1="90" x2="140" y2="150" style="stroke:#000; stroke-width:2"></line>'
+  )
 }
 
 export function addArm(element, side) {
-  const line = document.createElementNS(SVG_NS, 'line');
+  /*const line = document.createElementNS(SVG_NS, 'line');
   line.setAttribute('x1', '140');
   line.setAttribute('y1', '110');
   line.setAttribute('x2', side === 'left' ? '120' : '160');
   line.setAttribute('y2', '140');
   line.setAttribute('style', 'stroke: #000; stroke-width: 2;');
-  element.appendChild(line);
+  element.appendChild(line);*/
+  element.insertAdjacentHTML(
+    'beforeend',
+    `<line x1="140" y1="110" x2="${side === 'left' ? '120' : '160'}" y2="140" style="stroke:#000; stroke-width:2"></line>`
+  )
 }
 
 export function addLeg(element, side) {
-  const line = document.createElementNS(SVG_NS, 'line');
+  /*const line = document.createElementNS(SVG_NS, 'line');
   line.setAttribute('x1', '140');
   line.setAttribute('y1', '140');
   line.setAttribute('x2', side === 'left' ? '120' : '160');
   line.setAttribute('y2', '170');
   line.setAttribute('style', 'stroke: #000; stroke-width: 2;');
-  element.appendChild(line);
+  element.appendChild(line);*/
+  element.insertAdjacentHTML(
+    'beforeend',
+    `<line x1="140" y1="140" x2="${side === 'left' ? '120' : '160'}" y2="170" style="stroke:#000; stroke-width:2"></line>`
+  )
 }
 
 // Вызовите эту функцию, когда игрок допустит ошибку
-addHead();
+/*addHead();
 addBody();
 addArm('left');
 addArm('right');
 addLeg('left');
-addLeg('right');
+addLeg('right');*/
