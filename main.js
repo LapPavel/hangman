74d46cbed388c6e95c4fbf4d8171f99290602ced
LapPavel <118738689+LapPavel@users.noreply.github.com>
2024-04-26 20:08:50 +0300

    add build page

--- main.js
+++
@@ -1,7 +0,0 @@
-import './src/style/normalize.css';
-import './src/style/style.css';
-import { gallowsBox } from './src/js/gallowsBox';
-import { wordBox } from './src/js/wordBox';
-
-const appBody = document.querySelector('#app');
-appBody.prepend(gallowsBox, wordBox);
